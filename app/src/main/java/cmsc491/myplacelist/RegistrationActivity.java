4cb12c9722a8667634c6c7ecb4ec6114acc330d2
Chris McGee <cmcgee.csm@gmail.com>
2015-05-11 00:40:17 -0400

    Registration crash fix.

--- app/src/main/java/cmsc491/myplacelist/RegistrationActivity.java
+++ app/src/main/java/cmsc491/myplacelist/RegistrationActivity.java
@@ -23,8 +23,8 @@ public class RegistrationActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
         mEmail = (EditText) findViewById(R.id.emailField);
